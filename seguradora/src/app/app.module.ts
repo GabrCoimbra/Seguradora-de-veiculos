4c919d91c4cdfb227c5ca790aaf2ea53eed8ab3c
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-27 15:21:27 -0300

    Adicionando favIcons via angular

--- seguradora/src/app/app.module.ts
+++ seguradora/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
