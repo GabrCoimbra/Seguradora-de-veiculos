d017de2019ffcb242175e0a6500819db2138af35
Gabriel Coimbra <gab4.paina@gmail.com>
2019-09-28 19:23:44 -0300

    Config heroku

--- seguradora/server.js
+++ seguradora/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/seguradora'));
+app.use(express.static(__dirname + '/app/seguradora'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/seguradora/index.html'));
+res.sendFile(path.join(__dirname+'/app/seguradora/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
