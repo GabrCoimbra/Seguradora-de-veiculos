5d3426240c4c0dc7378ff91b81f4a573bde429c5
Gabriel Coimbar <gab4.paina@gmail.com>
2019-09-29 23:32:33 -0300

    Criando metodos para criacao e consulta das apí

--- seguradora/src/app/services/consultas.service.ts
+++ seguradora/src/app/services/consultas.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,16 @@ export class ConsultasService {
 
   constructor(private http:HttpClient) { }
   public getMarca():Observable<any>{
-    let url = `http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.d
-    ll/veiculo?cod_marca=99`; 
+    let url = `https://cors-anywhere.herokuapp.com/http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.dll/veiculo/marca`; 
     return this.http.get(url);
+  }
+  public getVeiculos(id):Observable<any>{
+    let url = `https://cors-anywhere.herokuapp.com/http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.dll/veiculo?cod_marca=`+id; 
+    return this.http.get(url);
+  }
+  public enviarDados(dados){
+    let options = { dados };
+    return this.http.post(`https://jsonplaceholder.typicode.com/posts`,options);
+  
   }
 }
---
+++ seguradora/src/app/services/listagem.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListagemService {
+
+  constructor(private http:HttpClient) { }
+
+  /**
+   * listagem dos usuarios 
+   */
+  public listagem() {
+    let url = `http://localhost:8080/seguradora/index.php`; 
+    return this.http.get(url);
+  }
+  public deletConsulta(id){
+    let url = `https://localhost:8080/delete?id=`+id; 
+    return this.http.get(url);
+  }
+  public descConsulta(id){
+    let url = `https://jsonplaceholder.typicode.com/users`;//+id; 
+    return this.http.get(url);
+  }
+}
