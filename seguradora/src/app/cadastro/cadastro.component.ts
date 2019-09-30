1e09e65884d88298020ad9060cfb2bd0f380e35b
Gabriel Coimbar <gab4.paina@gmail.com>
2019-09-29 23:29:37 -0300

    Alterando o metodo post para cadastro

--- seguradora/src/app/cadastro/cadastro.component.ts
+++ seguradora/src/app/cadastro/cadastro.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
 import { ConsultasService } from 'src/app/services/consultas.service';
 @Component({
   selector: 'app-cadastro',
@@ -9,24 +7,59 @@ import { ConsultasService } from 'src/app/services/consultas.service';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+  dadosMarca: any;
+  dadosVeiculo: any;
   form: FormGroup;
+  mensagem: any;
   constructor(private fb: FormBuilder, private ConsultasService: ConsultasService) {
     this.getter();
   }
 
+  //Verifica se há erro na validação do nome
   error(field:string){
     return this.form.get(field).errors
   }
+
+  //Busca as marcas cadastrada na API
   getter(){
     this.ConsultasService.getMarca().subscribe(data => {
-      console.log('Recebemos',data)
+      this.dadosMarca = data.marcas;
     }, erros=>{
 
     });
   }
+
+  //Buscando veiculo caso há uma marca selecionada
+  buscaVeiculos( marca: any ){
+    if(marca){
+      let id = marca.target.value;
+      this.ConsultasService.getVeiculos(id).subscribe(data => {
+        this.dadosVeiculo = data.veiculos;
+      }, erros=>{
+  
+      });
+    }
+  }
+  
+  //Recebe os dados do formulario 
+  enviarDados (){
+     const dadosForm = this.form.value;
+      this.ConsultasService.enviarDados(dadosForm).subscribe(data => {
+        this.mensagem = data;
+        console.log('teste',data);
+      }, erros => {
+        console.log(erros);
+      });
+    
+
+  }
   ngOnInit() {
     this.form = this.fb.group({
-      nome: [null, [ Validators.required, Validators.minLength(3), Validators.maxLength(50),  ] ]
+      nome: ['', [ Validators.required, Validators.minLength(3), Validators.maxLength(50),  ] ],
+      sexo: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      marca: ['', [Validators.required]],
+      veiculo:['', [Validators.required]]
     });
   }
 
