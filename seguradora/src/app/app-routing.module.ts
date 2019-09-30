4d23f4e3f1ca2fa8f761d870d7336336dc0673c3
Gabriel Coimbar <gab4.paina@gmail.com>
2019-09-29 23:33:37 -0300

    Criando rota de listagem

--- seguradora/src/app/app-routing.module.ts
+++ seguradora/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 const routes: Routes = [
+  { path:'listagem', component: ListagemComponent },
   { path:'cadastro', component: CadastroComponent },
   { path:'', component: MainComponent }
 ];
