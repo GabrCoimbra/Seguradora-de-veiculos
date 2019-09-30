3d5d70a58bc29fd42cd0ebe99a4eb239492ece79
Gabriel Coimbar <gab4.paina@gmail.com>
2019-09-29 23:30:51 -0300

    Modal de descricao das consultas

---
+++ seguradora/src/app/descricao/descricao.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { ListagemService } from 'src/app/services/listagem.service';
+import { VERSION, MatDialogRef, MatDialog, MatSnackBar, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-descricao',
+  templateUrl: './descricao.component.html',
+  styleUrls: ['./descricao.component.css']
+})
+export class DescricaoComponent implements OnInit {
+  id:number;
+  dados:any;
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any, private dialogRef: MatDialogRef<DescricaoComponent>, private ListagemService: ListagemService) { 
+    if (data) {
+        this.dados = this.ListagemService.descConsulta( this.id );
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
