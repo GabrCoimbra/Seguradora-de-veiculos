5d25244103a9483fdaa93267da21deae5134fcb9
Gabriel Coimbra <gab4.paina@gmail.com>
2019-10-02 16:50:08 -0300

    Construindo a rota de edição

--- seguradora/src/app/app-routing.module.ts
+++ seguradora/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListagemComponent } from './listagem/listagem.component';
+import { EditarComponent } from './editar/editar.component';
 
 const routes: Routes = [
   { path:'listagem', component: ListagemComponent },
   { path:'cadastro', component: CadastroComponent },
+  { path:'editar/:id', component: EditarComponent},
   { path:'', component: MainComponent }
 ];
 
